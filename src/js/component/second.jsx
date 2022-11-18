69ca5d54bfac81c90d91639cc632c9826dbc49f8
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-18 17:34:03 +0000

    Encaminado

--- src/js/component/second.jsx
+++ src/js/component/second.jsx
@@ -3,16 +3,17 @@ import React from "react";
 const Contador = (props) => {
 
     return (
-        
+
         <div className="container">
             <div className="calendar">
                 <i className="far fa-clock"></i>
             </div>
-            <div className="cuarto">{Math.floor(props.second/3600)}</div>
-            <div className="tercero">{Math.floor(props.second/60)}</div>
-            <div className="segundo">{Math.floor(props.second/3600)*60}</div>
+            <div className="cuarto">{Math.floor(props.second / 3600)}</div>
+            <div className="tercero">{Math.floor(props.second / 60)}</div>
+            <div className="segundo">{Math.floor(props.second / 3600) * 60}</div>
             <div className="primero">{Math.floor(props.second % 10)}</div>
-            
+
+
         </div>
 
     )
